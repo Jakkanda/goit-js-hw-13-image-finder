84541cf8458179c9225ffbbcd8c063221a5aad2f
Jakkanda <51709348+Jakkanda@users.noreply.github.com>
2021-05-25 00:08:08 +0300

    Project complited

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
-import debounce from 'lodash.debounce';
-const apiKey = '21755043-d9575a6749b270eceb64a1173';
+import PixabayApiService from './js/apiService';
+import galleryTemplate from './templates/gallery-template.hbs';
+import './sass/main.scss';
+
 const refs = {
   input: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
 };
 
-refs.input.addEventListener('input', debounce(onInputChange, 500));
+const pixabayApiService = new PixabayApiService();
 
-function onInputChange(e) {
-  const findSmth = e.target.value;
-  console.log(findSmth);
+refs.input.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  clearImagesContainer();
+  pixabayApiService.query = e.currentTarget.elements.query.value;
+  pixabayApiService.resetPage();
+
+  return pixabayApiService
+    .fetchImages()
+    .then(renderPage)
+    .catch(error => console.log(error));
+}
+
+function scrollToButton() {
+  refs.loadMoreBtn.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
+}
+
+function onLoadMore() {
+  return pixabayApiService
+    .fetchImages()
+    .then(renderPage)
+    .then(scrollToButton)
+    .catch(error => console.log(error));
+}
+
+function renderPage(hits) {
+  refs.gallery.insertAdjacentHTML('beforeend', galleryTemplate(hits));
+}
+
+function clearImagesContainer() {
+  refs.gallery.innerHTML = '';
 }
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,5 +1,34 @@
-const apiKey = '21755043-d9575a6749b270eceb64a1173';
-const refs = {
-  input: document.querySelector('.search-form'),
-};
-console.log(refs.input.value);
+export default class PixabayApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchImages() {
+    const apiKey = '21755043-d9575a6749b270eceb64a1173';
+    return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${apiKey}
+    `)
+      .then(response => response.json())
+      .then(data => {
+        this.incrementPage();
+
+        return data.hits;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
